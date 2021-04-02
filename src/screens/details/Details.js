ec42f1d831e2e023a12fa501441249361e0b0a91
Shobhit Srivastava <shobhit3107@gmail.com>
2021-04-02 20:42:23 +0530

    Added ‘Back to Home’ button and YouTube trailer on details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -3,6 +3,9 @@ import Header from "../../common/header/Header";
 import moviesData from "../../common/movieData";
 import Typography from "@material-ui/core/Typography";
 import "./Details.css";
+import Home from "../../screens/home/Home";
+import ReactDOM from "react-dom";
+import YouTube from "react-youtube";
 
 class Details extends Component {
   constructor() {
@@ -20,11 +23,27 @@ class Details extends Component {
     this.setState({ currentState });
   }
 
+  backtohomeHandler() {
+    ReactDOM.render(<Home />, document.getElementById("root"));
+  }
+
   render() {
     let movie = this.state.movie;
+    const opts = {
+      height: "300",
+      width: "700",
+      playerVars: {
+        autoplay: 1,
+      },
+    };
     return (
       <div className="details">
         <Header />
+        <div className="back">
+          <Typography onClick={this.backtohomeHandler}>
+            &#60; Back to Home
+          </Typography>
+        </div>
         <div className="flex-containerDetails">
           <div className="leftDetails">
             <img src={movie.poster_url} alt={movie.title} />
@@ -62,6 +81,16 @@ class Details extends Component {
                 <span className="bold">Plot:</span>
                 <a href={movie.wiki_url}>(Wiki Link)</a> {movie.storyline}
               </Typography>
+            </div>
+            <div className="trailerContainer">
+              <Typography>
+                <span className="bold">Trailer:</span>
+              </Typography>
+              <YouTube>
+                videoId={movie.trailer_url.split("?v=")[1]}
+                opts={opts}
+                onReady={this._onReady}
+              </YouTube>
             </div>
           </div>
           <div className="rightDetails"></div>
